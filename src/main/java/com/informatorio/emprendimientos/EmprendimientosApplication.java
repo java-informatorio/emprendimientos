1c5340845afc460b3b94bf0679f65c5b1618de39
Eduardo Ortiz <eduortiz.g@gmail.com>
2021-12-11 14:32:33 -0300

    EMP-001 - Agregados archivos base

---
+++ src/main/java/com/informatorio/emprendimientos/EmprendimientosApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.emprendimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprendimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprendimientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/emprendimientos/EmprendimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.emprendimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
